b58854fa0d612165874c6cecda1b37e70ad567b8
Eric Jackson <swiftgist@gmail.com>
2018-01-27 08:45:59 -0500

    Refactored into separate file
    Signed-off-by: Eric Jackson <swiftgist@gmail.com>

---
+++ src/ds.rs
@@ -0,0 +1,68 @@
+
+pub mod diskspace {
+    use std::iter::FromIterator;
+    use std::collections::BTreeMap;
+    use std::path::Path;
+    use std::os::linux::fs::MetadataExt;
+
+    pub fn traverse(mut directories: Vec<String>) -> BTreeMap<String, u64> {
+        let mut disk_space: BTreeMap<String, u64> = BTreeMap::new();
+
+        while directories.len() != 0 {
+            let dir = directories.pop().unwrap();
+            // println!("{:?}", dir);
+            let path = Path::new(&dir).read_dir().unwrap();
+            for entry in path {
+                if let Ok(entry) = entry {
+                    let pathname = entry.path().to_str().unwrap().to_string();
+                    if entry.path().is_dir() {
+                        directories.push(pathname);
+                    } else {
+                        let metadata = entry.metadata().expect("metadata call failed");
+                        disk_space.insert(pathname, metadata.st_size());
+                        let parent = entry.path().parent().unwrap().to_str().unwrap().to_string();
+                        let mut size: u64 = 0;
+                        if disk_space.contains_key(&parent) {
+                            size = *disk_space.get(&parent).unwrap();
+                        }
+                        size += metadata.st_size();
+                        disk_space.insert(parent, size);
+                    }
+                }
+            }
+        }
+        disk_space
+
+    }
+
+    pub fn report(disk_space: BTreeMap<String, u64>) {
+
+        let mut sorted = Vec::from_iter(disk_space);
+        sorted.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
+
+        let mut index = 0;
+        for (filename, size) in sorted {
+            if index > 19 { 
+                break;
+            }
+            println!("{} {}", simple_units(size), filename);
+            index += 1;
+        }
+
+    }
+
+    pub fn simple_units(number: u64) -> String {
+        let units = [ " ", "K", "M", "G", "T", "P" ];
+        let mut index = 0;
+        let mut n = number;
+        while n > 1024 {
+            index += 1;
+            n = n/1024;
+        }
+        if index == 0 {
+            format!("{:>6}", n)
+        } else {
+            format!("{:>5}{}", n, units[index])
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,19 @@
 
-use std::iter::FromIterator;
-use std::collections::BTreeMap;
-use std::path::Path;
-use std::os::linux::fs::MetadataExt;
-
-// struct DiskEntry {
-//     size: u64,
-//     count: u64,
-// }
-
+mod ds;
+use ds::diskspace;
+// use ds::diskspace::traverse;
+// use ds::diskspace::report;
 
 fn main() {
-    let mut disk_space: BTreeMap<String, u64> = BTreeMap::new();
     let mut directories = Vec::new();
 
     directories.push("./".to_string());
 
-    while directories.len() != 0 {
-        let dir = directories.pop().unwrap();
-        // println!("{:?}", dir);
-        let path = Path::new(&dir).read_dir().unwrap();
-        for entry in path {
-            if let Ok(entry) = entry {
-                let pathname = entry.path().to_str().unwrap().to_string();
-                if entry.path().is_dir() {
-                    directories.push(pathname);
-                } else {
-                    let metadata = entry.metadata().expect("metadata call failed");
-                    // disk_space.insert(pathname, metadata.st_size());
-                    let parent = entry.path().parent().unwrap().to_str().unwrap().to_string();
-                    let mut size: u64 = 0;
-                    if disk_space.contains_key(&parent) {
-                        size = *disk_space.get(&parent).unwrap();
-                    }
-                    size += metadata.st_size();
-                    disk_space.insert(parent, size);
-                }
-            }
-        }
-    }
+    let disk_space = diskspace::traverse(directories);
 
-    // for (filename, size) in &disk_space {
-    //     println!("{} {}", size, filename);
-    // }
+    diskspace::report(disk_space);
 
-    let mut sorted = Vec::from_iter(disk_space);
-    sorted.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
-
-    for (filename, size) in sorted {
-        println!("{:>9} {}", simple_units(size), filename);
-    }
-
+}
 
     // let start = Path::new("./");
     // println!("{:?}", start)
@@ -76,15 +39,64 @@ fn main() {
 //        }
 //        disk_space
 //    }
-}
 
-fn simple_units(number: u64) -> String {
-    let units = [ " ", "K", "M", "G", "T", "P" ];
-    let mut index = 0;
-    let mut n = number;
-    while n > 1024 {
-        index += 1;
-        n = n/1024;
-    }
-    format!("{:>5}{}", n, units[index])
-}
+// fn traverse(mut directories: Vec<String>) -> BTreeMap<String, u64> {
+//     let mut disk_space: BTreeMap<String, u64> = BTreeMap::new();
+// 
+//     while directories.len() != 0 {
+//         let dir = directories.pop().unwrap();
+//         // println!("{:?}", dir);
+//         let path = Path::new(&dir).read_dir().unwrap();
+//         for entry in path {
+//             if let Ok(entry) = entry {
+//                 let pathname = entry.path().to_str().unwrap().to_string();
+//                 if entry.path().is_dir() {
+//                     directories.push(pathname);
+//                 } else {
+//                     let metadata = entry.metadata().expect("metadata call failed");
+//                     disk_space.insert(pathname, metadata.st_size());
+//                     let parent = entry.path().parent().unwrap().to_str().unwrap().to_string();
+//                     let mut size: u64 = 0;
+//                     if disk_space.contains_key(&parent) {
+//                         size = *disk_space.get(&parent).unwrap();
+//                     }
+//                     size += metadata.st_size();
+//                     disk_space.insert(parent, size);
+//                 }
+//             }
+//         }
+//     }
+//     disk_space
+// 
+// }
+// 
+// fn report(disk_space: BTreeMap<String, u64>) {
+// 
+//     let mut sorted = Vec::from_iter(disk_space);
+//     sorted.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
+// 
+//     let mut index = 0;
+//     for (filename, size) in sorted {
+//         if index > 19 { 
+//             break;
+//         }
+//         println!("{} {}", simple_units(size), filename);
+//         index += 1;
+//     }
+// 
+// }
+// 
+// fn simple_units(number: u64) -> String {
+//     let units = [ " ", "K", "M", "G", "T", "P" ];
+//     let mut index = 0;
+//     let mut n = number;
+//     while n > 1024 {
+//         index += 1;
+//         n = n/1024;
+//     }
+//     if index == 0 {
+//         format!("{:>6}", n)
+//     } else {
+//         format!("{:>5}{}", n, units[index])
+//     }
+// }
